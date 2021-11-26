3a5c7787cd80635294cd33d74b34092fbf277ada
shamanthMuroor <shamanthm3@gmail.com>
2021-11-26 21:03:01 +0530

    Lab 4 - static progress bar & alert box

---
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.lab4;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+    }
+    public void Verify(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Confirm Exit");
+        alert.setCancelable(true);
+        alert.setMessage("Sure?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        alert.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Welcome back", Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_LONG).show();
+            }
+        });
+        AlertDialog ad = alert.create();
+        ad.show();
+    }
+}
